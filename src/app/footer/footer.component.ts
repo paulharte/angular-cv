c8e268339eecc3965155587abd5610e446be980e
Paul Harte <hartepaul@gmail.com>
2021-01-23 17:24:18 +0000

    Updating link! Paulspetprojects.net is live

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   github = 'http://github.com/paulharte/angular-cv'
-  live_link = 'http://angular-cv.s3-website-eu-west-1.amazonaws.com/'
+  live_link = 'https://cv.paulspetprojects.net/'
 
   constructor() { }
 
